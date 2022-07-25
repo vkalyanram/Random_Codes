9665476f6fe7b113089de3da64c9e2094447777c
KalyanRam <kalyan.2405@gmail.com>
2022-07-25 22:13:02 +0530

    Update Stack over flow.py

--- Stack over flow.py
+++ Stack over flow.py
@@ -1,6 +1,6 @@
 def search_dictionaries(key, value, list_of_dictionaries):
     return [element for element in list_of_dictionaries if element[key] == value]
-  
+----------------------------------------------------------------------------------------------------------  
   my_dict = [
     {"name": "Tom", "age": 10},
     {"name": "Mark", "age": 5},
