e51567ed8e079ccb2d3c65813ae9999ea7fd39dd
KalyanRam <kalyan.2405@gmail.com>
2022-07-25 19:57:04 +0530

    Create Stack over flow.py

---
+++ Stack over flow.py
@@ -0,0 +1,10 @@
+def search_dictionaries(key, value, list_of_dictionaries):
+    return [element for element in list_of_dictionaries if element[key] == value]
+  
+  my_dict = [
+    {"name": "Tom", "age": 10},
+    {"name": "Mark", "age": 5},
+    {"name": "Pam", "age": 7}
+]
+
+next(filter(lambda obj: obj.get('name') == 'Pam', my_dict), None)
