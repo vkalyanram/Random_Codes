c4e01fa2f14ca4ad1b2053d45417c79bb97e6494
KalyanRam <kalyan.2405@gmail.com>
2022-07-26 13:05:58 +0530

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,29 @@
+import smtplib, ssl
+
+class Mail:
+
+    def __init__(self):
+        self.port = 465
+        self.smtp_server_domain_name = "smtp.gmail.com"
+        self.sender_mail = ""
+        self.password = ""
+
+    def send(self, emails, subject, content):
+        ssl_context = ssl.create_default_context()
+        service = smtplib.SMTP_SSL(self.smtp_server_domain_name, self.port, context=ssl_context)
+        service.login(self.sender_mail, self.password)
+        
+        for email in emails:
+            result = service.sendmail(self.sender_mail, email, f"Subject: {subject}\n{content}")
+
+        service.quit()
+
+
+if __name__ == '__main__':
+    mails = input("Enter emails: ").split()
+    subject = input("Enter subject: ")
+    content = input("Enter content: ")
+
+    mail = Mail()
+    mail.send(mails, subject, content)
+
